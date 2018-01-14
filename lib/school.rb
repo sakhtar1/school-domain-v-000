a6ce9babd0377d14a9f7c3214a4362e894848c0c
Sharmina Akhtar <sakht318@gmail.com>
2018-01-14 11:57:32 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,8 +8,12 @@ class School
   end
 
   def add_student(name,grade)
+    if roster[grade] == nil
       roster[grade]=[]
       roster[grade] << name
+    else
+      roster[grade] << name
+    end
   end
 
   def grade(grade)
